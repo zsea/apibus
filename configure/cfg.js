e5dec23f174f26f59b52ed5a8073feed8535e957
zsea.home <zsea@home>
2018-03-16 23:22:42 +0800

    更新配置加载文件

--- configure/cfg.js
+++ configure/cfg.js
@@ -3,27 +3,27 @@ module.exports = {
         domain: ["*"],
     },
     REDIS: {
-        host: "127.0.0.1",
-        port: 6379,
-        //password:""
+        host: process.env["APIBUS_REDIS_HOST"] || "127.0.0.1",
+        port: process.env["APIBUS_REDIS_PORT"] || 6379,
+        password: process.env["APIBUS_REDIS_PASSWORD"] || undefined,
         db: process.env["APIBUS_REDIS_NUMBER"] || 0
     },
     MYSQL: {
-        host: "127.0.0.1",
-        port: 3306,
-        user: "root",
-        password: "",
-        database: "apibus",
-        connectionLimit: 8
+        host: process.env["APIBUS_MYSQL_HOST"] || "127.0.0.1",
+        port: process.env["APIBUS_MYSQL_PORT"] || 3306,
+        user: process.env["APIBUS_MYSQL_USER"] || "root",
+        password: process.env["APIBUS_MYSQL_PASSWORD"] || "",
+        database: process.env["APIBUS_MYSQL_DBNAME"] || "apibus",
+        connectionLimit: process.env["APIBUS_MYSQL_POOLSIZE"] || 8
     },
     BODY: {}//bodyParse转换配置，详见：koa-better-body说明
     , ADMIN: {
-        APPKEY: 0,
-        SECRET: 0
+        APPKEY: process.env["APIBUS_ADMIN_APPKEY"] || 0,
+        SECRET: process.env["APIBUS_ADMIN_SECRET"] || 0
     },
     CLUSTER: {
-        TOKEN: "1",
-        MANAGER: process.env["APIBUS_MANAGER"]=="true",
-        NODENAME: process.env["APIBUS_NODE_NAME"] || "master"
+        TOKEN: process.env["APIBUS_CLUSTER_TOKEN"],
+        MANAGER: process.env["APIBUS_CLUSTER_MANAGER"] == "true",
+        NODENAME: process.env["APIBUS_CLUSTER_NODENAME"] || "master"
     }
 }
